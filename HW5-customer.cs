0ff356c57b5b68028502d46e51c4cefdf8a00997
jbtran1 <157447397+jbtran1@users.noreply.github.com>
2024-07-15 21:43:48 -0500

    Add files via upload

---
+++ HW5-customer.cs
@@ -0,0 +1,38 @@
+﻿namespace homework5;
+
+using System;
+public class Customer
+{
+    private int cus_id;
+    public string cus_name;
+    public int cus_age;
+
+    public Customer(int cus_id, string cus_name, int cus_age)
+    {
+        this.cus_id = cus_id;
+        this.cus_name = cus_name;
+        this.cus_age = cus_age;
+    }
+
+    public void ChangeID(int new_id)
+    {
+        cus_id = new_id;
+    }
+
+    public void PrintCusInfo()
+    {
+        Console.WriteLine($"Customer: {cus_id}, name: {cus_name}, age: {cus_age}");
+    }
+
+    public void CompareAge(Customer objCustomer)
+    {
+        if (this.cus_age > objCustomer.cus_age)
+        {
+            Console.WriteLine($"{this.cus_name} is older");
+        }
+        else if (this.cus_age < objCustomer.cus_age)
+        {
+            Console.WriteLine($"{objCustomer.cus_name} is older");
+        }
+    }
+}
---
+++ HW5-program.cs
@@ -0,0 +1,24 @@
+﻿namespace homework5;
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Customer customer1 = new Customer(110, "Alice", 28);
+        Customer customer2 = new Customer(111, "Bob", 30);
+
+
+        customer1.PrintCusInfo();
+        customer2.PrintCusInfo();
+
+        customer1.ChangeID(220);
+        customer2.ChangeID(221);
+
+
+        customer1.PrintCusInfo();
+        customer2.PrintCusInfo();
+
+        customer1.CompareAge(customer2);
+    }
+}
